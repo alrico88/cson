74f0cdca210335c6566f57fbfd842b7c10bc8cc3
Alberto Rico <alrico88@gmail.com>
2023-08-16 18:26:23 +0200

    fix: added meta description for index

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,19 @@
   p With CSON you can convert from CSV to JSON or from JSON to CSV free, fast, and privately. It even works offline!
   p.lead.fw-bold.mb-0 Please select a conversion in the menu above
 </template>
+
+<script setup lang="ts">
+useHead({
+  meta: [
+    {
+      name: "description",
+      content:
+        "Progressive Web App to convert between CSV and JSON formats with privacy and ease of use in mind",
+    },
+    {
+      name: "keywords",
+      content: ["csv", "json", "convert"],
+    },
+  ],
+});
+</script>
